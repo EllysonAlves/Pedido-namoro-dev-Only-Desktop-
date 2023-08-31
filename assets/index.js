60b96f0ac4dcd97666ba7b36971bf16a3746352e
Ellyson Alves <ellyson.alves.ds@gmail.com>
2023-08-31 16:29:11 -0300

    frist commit

---
+++ assets/index.js
@@ -0,0 +1,41 @@
+document.getElementById("btn-sim").addEventListener("click", alerta);
+const nao = document.getElementById("btn-nao");
+nao.addEventListener("mouseenter", entrar);
+
+
+
+function alerta(){
+    alert("HUMMM, ENTÃO VOCE ESTÀ INTERESSADA !");
+}
+
+let estado = -1;
+
+function entrar(){
+    estado++
+    if (estado == 0){
+        nao.style.transform="translate(130px, -150px)"
+    }
+    if (estado == 1){
+        nao.style.transform="translate(130px, +150px)"
+    }
+    if (estado == 2){
+        nao.style.transform="translate(-325px, +150px)"
+    }
+    if (estado == 3){
+        nao.style.transform="translate(-325px, -150px)"
+    }
+    if (estado == 4){
+        nao.style.transform="translate(+135px, +150px)"
+    }
+    if (estado == 5){
+        nao.style.transform="translate(+135px, -150px)"
+    }
+    if (estado == 6){
+        nao.style.transform="translate(-325px, +150px)"
+    }
+    if (estado == 7){
+        nao.style.transform="translate(-325px, -150px)"
+        estado = -1
+    }
+}
+
